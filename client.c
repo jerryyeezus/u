2b3017d0890f03ec745bc7ed54e83e3a97c6b343
jerryyeezus <yee@gatech.edu>
2013-11-20 14:44:34 -0500

    merge

--- client.c
+++ client.c
@@ -10,8 +10,8 @@
 #define TMPBUFSIZE 512		    /* Size for temporary buffers */
 #define INPUTSIZE 12
 
-//char *servIP = "130.207.114.21";
-char *servIP = "localhost";
+char *servIP = "130.207.114.21";
+//char *servIP = "localhost";
 
 
 /* The main function */
