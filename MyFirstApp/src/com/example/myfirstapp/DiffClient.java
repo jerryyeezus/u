9dba54a3b09ed9f3d642d27d3b3e9163b6f1a095
jerryyeezus <yee@gatech.edu>
2013-11-25 13:29:05 -0500

    fixed diff

--- MyFirstApp/src/com/example/myfirstapp/DiffClient.java
+++ MyFirstApp/src/com/example/myfirstapp/DiffClient.java
@@ -8,68 +8,70 @@ import android.util.Log;
 
 public class DiffClient {
 
-	Message rcvMessage;
-	
-	public DiffClient(Message rcvMessage) {
-		rcvMessage = this.rcvMessage;
+    Message rcvMessage;
+
+    public DiffClient(Message rcvMessage) {
+	rcvMessage = this.rcvMessage;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] fileCompare(Message rcvMessage) {
+
+	ArrayList<String> diffList = new ArrayList<String>();
+	boolean isFound;
+
+	/* Get server file names from received message */
+	String[] serverNames = rcvMessage.getFileNameArray();
+
+	/* Get client file names from local directory */
+	String musicDir = Environment.getExternalStoragePublicDirectory(
+		Environment.DIRECTORY_MUSIC).toString();
+	File clientFiles = new File(musicDir);
+	String[] clientNames = getLocalFiles(clientFiles);
+
+	for (int i = 0; i < serverNames.length; i++) {
+	    isFound = false;
+	    //Log.d("debugging", "SL: " + (serverNames.length - 1));
+	    for (int j = 0; j < clientNames.length; j++) {
+		Log.d("debugging", "Server: " + serverNames[j]);
+		if (clientNames[i].equals(serverNames[j])) {
+		    isFound = true;
+		    break;
+		}
+	    }
+	    if (!isFound) {
+		diffList.add(serverNames[i]);
+	    }
 	}
 
-	public static void main(String[] args) {
-		
+	String[] diffNames = new String[diffList.size()];
+	for (int i = 0; i < diffNames.length; i++) {
+	    diffNames[i] = diffList.get(i).toString();
 	}
+
+	return diffNames;
+    }
+
+    public static String[] getLocalFiles(final File folder) {
+	ArrayList<String> fileList = new ArrayList<String>();
+	if (folder.listFiles() == null)
+	    return new String[0];
 	
-	public static String[] fileCompare(Message rcvMessage) {
-		
-		ArrayList <String> diffList = new ArrayList <String>();
-		boolean isFound;
-		
-		/* Get server file names from received message */
-		String[] serverNames = rcvMessage.getFileNameArray();
-		
-		/* Get client file names from local directory */
-		String musicDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).toString();
-		File clientFiles = new File(musicDir);
-	    String[] clientNames = getLocalFiles(clientFiles);
-	    
-	    for(int i = 0; i < clientNames.length; i++) {
-	    	isFound = false;
-	    	Log.d("debugging", "Client: " + clientNames[i]);
-	    	Log.d("debugging", "SL: " + (serverNames.length-1));
-	    	for(int j = 0; j < serverNames.length; j++) {
-	    		Log.d("debugging", "Server: " + serverNames[j]);
-	    		if(clientNames[i].equals(serverNames[j])) {
-	    			isFound = true;
-	    			break;
-	    		}
-	    	}
-	    	if(!isFound) {
-	    		Log.d("debugging", "No match found, diff: " + clientNames[i]);
-	    		diffList.add(clientNames[i]);
-	    	}	
+	for (final File fileEntry : folder.listFiles()) {
+	    if (fileEntry.isDirectory()) {
+		getLocalFiles(fileEntry);
+	    } else {
+		fileList.add(fileEntry.getName());
 	    }
-	    
-	    String[] diffNames = new String[diffList.size()];
-	    for(int i = 0; i < diffNames.length; i++) {
-	    	diffNames[i] = diffList.get(i).toString();
-	    }
-	    
-		return diffNames;
 	}
-	
-	public static String[] getLocalFiles(final File folder) {
-		ArrayList<String> fileList = new ArrayList<String>();
-        for (final File fileEntry : folder.listFiles()) {
-            if (fileEntry.isDirectory()) {
-                getLocalFiles(fileEntry);
-            } else {
-            	fileList.add(fileEntry.getName());
-            }
-        }
-        String[] fileNames = new String[fileList.size()];
-        for(int i = 0; i < fileNames.length; i++) {
-        	fileNames[i] = fileList.get(i).toString();
-        }
-        return fileNames;
+	String[] fileNames = new String[fileList.size()];
+	for (int i = 0; i < fileNames.length; i++) {
+	    fileNames[i] = fileList.get(i).toString();
+	}
+	return fileNames;
     }
 
 }
--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -19,100 +19,111 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
-	
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-	private Socket clientSock = null;
-	
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private Socket clientSock = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.main, menu);
+	return true;
     }
-    
+
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-    	
-    	//Create intent for sending message to DisplayMessageActivity
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
 
-    	//Get message from text box
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String str = editText.getText().toString();
-		
-		//Send message to server and receive transformed message
-		String message = null;
-		try {
-			message = new TCPSendMessage().execute(str).get();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
-		
-		//Put message into intent and send to DisplayMessageActivity
-		intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
+	// Create intent for sending message to DisplayMessageActivity
+	Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+	// Get message from text box
+	EditText editText = (EditText) findViewById(R.id.edit_message);
+	String str = editText.getText().toString();
+
+	// Send message to server and receive transformed message
+	String message = null;
+	try {
+	    message = new TCPSendMessage().execute(str).get();
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	} catch (ExecutionException e) {
+	    e.printStackTrace();
+	}
+
+	// Put message into intent and send to DisplayMessageActivity
+	intent.putExtra(EXTRA_MESSAGE, message);
+	startActivity(intent);
     }
-    
+
     private class TCPSendMessage extends AsyncTask<String, Void, String> {
-    	
-    	protected String doInBackground(String... arg0) {
-    		try {
-    			//Create a new socket
-    			clientSock = new Socket("130.207.114.21", 12003);
-    			
-    			//Write the message as an output stream
-    			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSock.getOutputStream())), true);
-    			Message commandMessage = new Message(arg0[0]);
-        		String encodedMessage = new String();
-        		encodedMessage = Message.encodeMessage(commandMessage);
-        		//Log.d("encoding", encodedMessage);
-    			out.println(encodedMessage);
-    			Log.d("debugging", "Message sent");
-        		
-        		//Receive the message from the server in unsigned bytes
-				DataInputStream input = new DataInputStream(clientSock.getInputStream());
-        		String str = new String();
-        		for(int i = 0; i < 2048; i++) {
-        			str += Character.toString((char)input.readUnsignedByte());
-        		}
-        		Message rcvMessage = Message.decodeMessage(str);
-        		Log.d("debugging", rcvMessage.toString());
-        		
-        		String[] diffNames =	 DiffClient.fileCompare(rcvMessage);
-        	    //Log.d("debugging", diffNames);
-        		
-        		String diffStr = new String();
-        		for(int i = 0; i < diffNames.length; i++) {
-        			Log.d("debugging", diffNames[i]);
-        			diffStr += diffNames[i] + "\n";
-        		}
-        		
-        		return "Differing Files: \n" + diffStr;
-        		//return "Server Files: \n" + rcvMessage.getFileNames() + "Client Files: \n" + diffNames;
-    		}
-    		catch(UnknownHostException e) {
-        		e.printStackTrace();
-        		Log.d("debugging", "Unknown Host Exception");
-        	}
-        	catch(IOException e) {
-        		e.printStackTrace();
-        		Log.d("debugging", "IO Exception");
-        	}
-			return null;
-    	}
-    	
-    	protected void onPreExecute() {}
-    	protected void onPostExecute(String strOut) {}
-    	protected void onProgressUpdate(Void... values) {}
-    	
+
+	protected String doInBackground(String... arg0) {
+	    try {
+		// Create a new socket
+		clientSock = new Socket("130.207.114.21", 12003);
+
+		// Write the message as an output stream
+		PrintWriter out = new PrintWriter(new BufferedWriter(
+			new OutputStreamWriter(clientSock.getOutputStream())),
+			true);
+		Message commandMessage = new Message(arg0[0]);
+		String encodedMessage = new String();
+		encodedMessage = Message.encodeMessage(commandMessage);
+		out.println(encodedMessage);
+
+		// Receive the message from the server in unsigned bytes
+		DataInputStream input = new DataInputStream(
+			clientSock.getInputStream());
+		//String str = new String();
+		StringBuilder str = new StringBuilder();
+		for (int i = 0; i < 2048; i++) {
+		    //str += Character.toString((char) input.readUnsignedByte());
+		    str.append((char) input.readUnsignedByte());
+		}
+		Message rcvMessage = Message.decodeMessage(str.toString());
+
+		Log.d("debugging", rcvMessage.toString());
+
+		String[] diffNames = DiffClient.fileCompare(rcvMessage);
+
+		String diffStr = new String();
+		String sizeStr = new String();
+		for (int i = 0; i < diffNames.length; i++) {
+		    for (int j = 0; j < 2048; j++) {
+			int tmp = input.readUnsignedByte();
+		    }
+
+		    diffStr += diffNames[i] + "\n";
+		}
+
+		return "Differing Files: \n" + diffStr;
+		// return "Server Files: \n" + rcvMessage.getFileNames() +
+		// "Client Files: \n" + diffNames;
+	    } catch (UnknownHostException e) {
+		e.printStackTrace();
+		Log.d("debugging", "Unknown Host Exception");
+	    } catch (IOException e) {
+		e.printStackTrace();
+		Log.d("debugging", "IO Exception");
+	    }
+	    return null;
+	}
+
+	protected void onPreExecute() {
+	}
+
+	protected void onPostExecute(String strOut) {
+	}
+
+	protected void onProgressUpdate(Void... values) {
+	}
+
     }
-    
+
 }
